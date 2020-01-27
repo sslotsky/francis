918a33adc1b509704b56bc02a85a3f82e1b1620d
Sam Slotsky <sam@manifold.co>
2020-01-27 09:09:20 -0600

    Keyboard display

---
+++ src/components/scott-free/scott-free.tsx
@@ -0,0 +1,56 @@
+import { Component, h } from "@stencil/core";
+
+@Component({
+  tag: "scott-free",
+  styleUrl: "scott-free.css",
+  shadow: true
+})
+export class MyComponent {
+  render() {
+    return (
+      <ul>
+        <li class="white c" />
+        <li class="black" />
+        <li class="white d" />
+        <li class="black" />
+        <li class="white e" />
+        <li class="white f" />
+        <li class="black" />
+        <li class="white g" />
+        <li class="black" />
+        <li class="white a" />
+        <li class="black" />
+        <li class="white b" />
+        <li class="white c" />
+        <li class="black" />
+        <li class="white d" />
+        <li class="black" />
+        <li class="white e" />
+        <li class="white f" />
+        <li class="black" />
+        <li class="white g" />
+        <li class="black" />
+        <li class="white a" />
+        <li class="black" />
+        <li class="white b" />
+        <li class="white c" />
+        <li class="black" />
+        <li class="white d" />
+        <li class="black" />
+        <li class="white e" />
+        <li class="white f" />
+        <li class="black" />
+        <li class="white g" />
+        <li class="black" />
+        <li class="white a" />
+        <li class="black" />
+        <li class="white b" />
+        <li class="white c" />
+        <li class="black" />
+        <li class="white d" />
+        <li class="black" />
+        <li class="white e" />
+      </ul>
+    );
+  }
+}
---
+++ stencil.config.ts
@@ -0,0 +1,32 @@
+import { Config } from "@stencil/core";
+import { postcss } from "@stencil/postcss";
+import postCSSPresetEnv from "postcss-preset-env";
+
+export const config: Config = {
+  namespace: "francis",
+  plugins: [
+    postcss({
+      plugins: [
+        postCSSPresetEnv({
+          features: {
+            "custom-media-queries": true,
+            "nesting-rules": true
+          }
+        })
+      ]
+    })
+  ],
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader"
+    },
+    {
+      type: "docs-readme"
+    },
+    {
+      type: "www",
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
